53ab31d5908fe2578b4d387c4145931ad6c4ce60
Soumen Mandal <soumenmandal1080@gmail.com>
2026-03-05 00:10:07 +0530

    update styles in index.html and refactor main.js for improved rendering; add VSCode settings

--- main.js
+++ main.js
@@ -1,28 +1,41 @@
 import * as THREE from 'three';
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
+scene.add(camera);
 camera.position.z = 5;
 
-function animate( time ) {
 
-  cube.rotation.x = time / 2000;
-  cube.rotation.y = time / 100;
+/* MESH => (shspe, material) */
 
-  renderer.render( scene, camera );
-  
-   requestAnimationFrame( animate );
+const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 
+const cubeMaterial = new THREE.MeshStandardMaterial({
+    color: 0x00ff00
+});
+
+ const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+scene.add(cube);
+
+
+const Light = new THREE.DirectionalLight(0xffffff, 5);
+Light.position.y = 4;
+Light.position.z = 4;
+scene.add(Light);
+
+
+const renderer = new THREE.WebGLRenderer();
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+document.body.appendChild(renderer.domElement);
+
+
+
+function animate() {
+  renderer.render(scene, camera);
 }
-animate();
+  renderer.setAnimationLoop(animate);
