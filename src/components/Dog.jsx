f7bd31ea1ada8ee4ef377a41084ca0c4e4ebbc5c
Soumen Mandal <soumenmandal1080@gmail.com>
2026-03-06 23:07:40 +0530

    add Dog component with Canvas setup in Dog.jsx

---
+++ src/components/Dog.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+
+const Dog = () => {
+  return (
+
+      <Canvas>
+        
+      </Canvas>
+    
+  )
+}
+
+export default Dog
