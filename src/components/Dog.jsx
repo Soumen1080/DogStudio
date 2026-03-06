fa8f63f88707b5f71170905f79a53ae907ba50b4
Soumen Mandal <soumenmandal1080@gmail.com>
2026-03-06 23:41:39 +0530

    add OrbitControls to Dog component for enhanced scene navigation

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useThree } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
 
 const Dog = () => {
   const { camera, scene, gl } = useThree()
@@ -9,10 +10,17 @@ const Dog = () => {
   console.log(gl)
 
   return (
+
+<>
+
     <mesh>
       <boxGeometry args={[1, 1, 1]} />
       <meshBasicMaterial color="#00ff00" />
     </mesh>
+{/* orbit controls allow us to move around the scene with our mouse, we can zoom in and out and rotate around the scene */}
+    <OrbitControls />
+
+    </>
   )
 }
 
