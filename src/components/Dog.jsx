a7d71719a1a34bf2d4326763e9930ae2ceb7899a
Soumen Mandal <soumenmandal1080@gmail.com>
2026-03-12 04:33:07 +0530

    refactor Dog component: enhance animation handling and optimize texture loading for improved rendering

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
-import { OrbitControls, useGLTF, Environment, useTexture } from '@react-three/drei'
+import React , { useEffect } from 'react'
+import { OrbitControls, useGLTF, Environment, useTexture , useAnimations} from '@react-three/drei'
 import { Canvas , useThree  } from '@react-three/fiber'
+
 import * as THREE from 'three'
 
 const Dog = () => {
   const model = useGLTF('/models/dog.drc.glb')
   const { camera } = useThree()
   
-  // Load textures
-  const textures = useTexture({
-    normalMap: '/models/dog_normals.jpg',
-    sampleMatCap: '/matcap/mat-2.png',
-  })
-  
-    textures.normalMap.flipY = false  // Flip Y for correct normal map orientation
-    textures.sampleMatCap.colorSpace = THREE.SRGBColorSpace  // Ensure matcap texture is in sRGB color space for correct colors
+const { actions } =  useAnimations(model.animations , model.scene)
+
+  useEffect(() => {
+
+  actions["Take 001"].play() // Play the animation named "Take 001"
+}, [ actions ])  
 
 
+      const [normalMap,sampleMatCap1 ,] = useTexture(['/models/dog_normals.jpg','/matcap/mat-2.png','/matcap/mat-7.png' ,'/matcap/mat-8.png' ]).map((texture) => {
+      texture.flipY = false  // Flip Y for correct normal map orientation
+      texture.colorSpace = THREE.SRGBColorSpace  // Ensure textures are in sRGB color space for correct colors
+      return texture
+    })
 
 
   // Set camera to proper viewing distance
@@ -26,22 +30,22 @@ const Dog = () => {
   }, [camera])
 
 
-
+   const dogMaterial = new THREE.MeshMatcapMaterial({
+    matcap: sampleMatCap1, // Use the loaded matcap texture
+    normalMap: normalMap,
+    flatShading: false, // Use smooth shading for better appearance
+  })
 
 
   // Apply custom material with matcap to dog parts
   React.useEffect(() => {
     model.scene.traverse((child) => {
       if (child.isMesh && child.name.includes("DOG")) {
-        child.material = new THREE.MeshMatcapMaterial({
-          matcap: textures.sampleMatCap,  // Apply matcap for stylized shading and color
-          normalMap: textures.normalMap,
-          normalScale: new THREE.Vector2(1, 1),  // Adjust normal map intensity
-        })
+        child.material = dogMaterial
       }
     })
-  }, [model, textures])
-  
+  }, [model, normalMap, sampleMatCap1 , ]) // Re-apply material if textures change
+
 
 
 
@@ -54,7 +58,7 @@ const Dog = () => {
       <ambientLight intensity={1} />
       
       {/* Main directional light (sun) - softer intensity */}
-      <directionalLight position={[5, 5, 5]} intensity={1} castShadow />
+      <directionalLight position={[25, 25, 25]} intensity={1} castShadow />
       
       
          
